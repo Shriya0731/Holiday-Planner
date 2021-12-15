6f60ba51572652bf4c4ddb357e4ce4b3918a2b9e
Shriya Deshpande <91329086+Shriya0731@users.noreply.github.com>
2021-12-15 17:41:45 +0530

    Add files via upload

---
+++ Holiday_Planner.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Tour_Package{
+	int price;
+	String [] Destinations;
+	int days, capacity;
+	Tour_Package low , high;
+	
+}
+class Travel_Company{   //Binary search Tree
+	Tour_Package root;
+	int bydays,byprice;  // 
+	void create_price() {
+		
+	}
+	void create_days() {
+		
+	}
+	void search() { 
+		if (bydays==1) {  //To search based on number of days
+			
+		}
+		else if(byprice==1) { // To search based on budget
+			
+		}
+	}
+	void display() { // To display contents of one tourpackage
+		
+	}
+}
+public class Holiday_Planner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Travel_Company Marvel = new Travel_Company();
+        
+	}
+
+}
