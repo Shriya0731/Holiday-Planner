18794a861c978baa5668239ae9025eee4c14dcc8
Shriya Deshpande <91329086+Shriya0731@users.noreply.github.com>
2021-12-15 22:28:56 +0530

    Update Holiday_Planner.java

--- Holiday_Planner.java
+++ Holiday_Planner.java
@@ -16,8 +16,33 @@ class Travel_Company{   //Binary search Tree
 	void create_price() {
 		
 	}
-	void create_days() {
-		
+	void create_days(Tour_Package new_tour) {
+		if (root1==null) {
+			root1 = new_tour;
+		}
+		else {
+			ptr=root1;
+			while (ptr!=null) {
+				if (ptr.days > new_tour.days) {
+					if (ptr.low==null) {
+						ptr.low = new_tour;
+						break;
+					}
+					else {
+						ptr=ptr.low;
+					}
+				}
+				else {
+					if(ptr.high==null) {
+						ptr.high=new_tour;
+						break;
+					}
+					else {
+						ptr=ptr.high;
+					}
+				}
+			}
+		}
 	}
 	void searchByPrice(int searchPrice) {
 		
@@ -27,17 +52,23 @@ class Travel_Company{   //Binary search Tree
 	void searchByDays(int searchDays) { 
 		Scanner sc = new Scanner(System.in);
 		//To search based on number of day
-			if (ptr == null) {
+		ptr = root;	
+		if (ptr == null) {
 				System.out.println("Package you are looking for is not available.......\nPlease try out other package!");
 			}
-			ptr = root;
+			
 			while (ptr != null) {
 				if (ptr.days == 0) {
 					System.out.println("Package Not Available For This Input........ ");
 					
 				} else if (searchDays<ptr.days ) {
 					ptr = ptr.low;
-				} else
+				}
+				else if(searchDays == ptr.days) {
+					display(ptr);
+					break;
+				}
+				else
 					ptr = ptr.high;
 
 			}
