ea48d74adcc4b096cec734c57071ad36a4e43eb6
tanvibhosale16 <89827357+tanvibhosale16@users.noreply.github.com>
2021-12-26 15:51:22 +0530

    Update Category.java

--- Category.java
+++ Category.java
@@ -1,10 +1,9 @@
 public class Category{
-		Tour_Package new_package;
-		Category low , high;
-		Category(Tour_Package tour){
-			this.new_package = tour;
-			this.new_package.same = null;
-			low = high = null;
-		}
-	}
-
+    Tour_Package new_package;
+    Category low , high;
+    Category(Tour_Package tour){
+        this.new_package = tour;
+        this.new_package.same = null;
+        low = high = null;
+    }
+}
